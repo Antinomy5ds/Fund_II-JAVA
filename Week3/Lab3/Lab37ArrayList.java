84d97a06c2ff7d6e98e1e476e305515d48aaba38
Tehapuet Weerawat <84505108+Antinomy5ds@users.noreply.github.com>
2022-12-29 17:59:47 +0700

    Add files via upload

---
+++ Week3/Lab3/Lab37ArrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Lab37ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        System.out.println(arrayList);
+        arrayList.add(23);
+        arrayList.add(28);
+        arrayList.add(35);
+        arrayList.add(21);
+        arrayList.add(19);
+        System.out.println(arrayList);
+    }
+}
