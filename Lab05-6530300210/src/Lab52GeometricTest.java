44971b5d52ed7b0e46447edddfe4d5eba6fa1678
Antinomy5ds <84505108+Antinomy5ds@users.noreply.github.com>
2023-03-11 12:53:52 +0700

    Add files via upload

---
+++ Lab05-6530300210/src/Lab52GeometricTest.java
@@ -0,0 +1,10 @@
+
+public class Lab52GeometricTest {
+    public static void main(String[] args) {
+        Circle c = new Circle(5.0);
+        System.out.println(c.toString());
+        Rectangle r = new Rectangle(2.0, 4.0);
+        System.out.println(r.toString());
+
+    }
+}
